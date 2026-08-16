909012e945fd5eeeb9550524a57290f2c2905a3d
Muhammad Sufyan Siddiqui <112391971+sufyansidd19@users.noreply.github.com>
2026-08-16 17:11:13 +0500

    edit file

--- Travel Agent MCP & HILP/mcp_client.py
+++ Travel Agent MCP & HILP/mcp_client.py
@@ -83,7 +83,6 @@ async def call_tool(tool_name:str,args:dict=None):
     return await tool.ainvoke(args or {})
 
 
-
 # ----------------
 # Tavily MCP Tools 
 #-----------------
@@ -103,128 +102,6 @@ async def current_weather(city:str):
 async def forecast(city:str):
     return await call_tool("get_forecast",{"city":city})
 
-
-
-
-# search_tool = None
-# aviation_tools={}
-# async def initialize_mcp():
-#     global search_tool
-#     global aviation_tools
-#     if search_tool is not None and aviation_tools:
-#         return
-    
-#     tools= await client.get_tools()
-    
-#     print("Available MCP tools:\n")
-    
-#     for tool in tools:
-#         print(tool.name)
-#     search_tool = next(
-#         tool for tool in tools if tool.name == "tavily_search"
-#         )        
-#     aviation_tools = {tool.name: tool for tool in tools 
-#                       if tool.name!="tavily_search"
-#                       }
-        
-# async def main():
-#     tools = await client.get_tools()
-#     search_tool = next(tool for tool in tools if tool.name == "tavily_search"
-#     )
-#     result = await search_tool.ainvoke(
-#         {"query": "What is the capital of France?"}
-#         )
-#     print(f"Search result: {result}")
-
-
-# async def tavily_mcp_search(query: str):
-#     await initialize_mcp()
-#     result = await search_tool.ainvoke(
-#         {"query": query}
-#     )
-#     return result
-
-# async def aviation_mcp_call(
-#     tool_name: str,
-#     tool_args: dict=None
-#     ):
-#     tools = await client.get_tools()
-#     tool=next((t for t in tools if t.name==tool_name))
-#     result = await tool.ainvoke(tool_args or {})
-#     return result
-
-# async def get_airports():
-#     await initialize_mcp()
-#     tool=aviation_tools.get("list_airports")
-    
-#     if not tool:
-#         return "Airports tool unavailable"
-    
-#     result=await tool.ainvoke({})
-#     return result
-
-# async def get_airlines():
-#     await initialize_mcp()
-#     tool=aviation_tools.get("list_airlines")
-    
-#     if not tool:
-#         return "Airlines tool unavailable"
-    
-#     result=await tool.ainvoke({})
-#     return result
-    
-    
-# weather_tools=None
-# forecast_tool=None
-
-# async def initialize_weather_tools():
-#     global weather_tools
-#     global forecast_tool
-#     if weather_tools is not None and forecast_tool is not None:
-#         return
-    
-#     tools= await client.get_tools()
-    
-#     print("Available MCP tools:\n")
-        
-#     weather_tools = next(
-#         tool for tool in tools if tool.name == "get_current_weather"
-#         )
-    
-#     forecast_tool = next(
-#         tool for tool in tools if tool.name == "get_forecast"
-#         )
-
-# async def weather_mcp_search(city: str):
-    
-#     await initialize_weather_tools()
-    
-#     result = await weather_tools.ainvoke(
-#         {"city": city}
-#     )
-
-# async def forecast_mcp_search(city: str):
-    
-#     await initialize_weather_tools()
-    
-#     result = await forecast_tool.ainvoke(
-#         {"city": city}
-#     )
-
-# from langchain_groq import ChatGroq
-
-# llm=ChatGroq(
-#     model="llama-3.3-70b-versatile")
-
-# def extract_destination(query: str):
-#     prompt=f"""
-#     Extract the destination city from the following user query:
-#     Query:{query}
-
-#     Return only Destination name.
-#     """
-#     response=llm.invoke(prompt)
-#     return response.content.strip()
 
  
 # if __name__ == "__main__":
